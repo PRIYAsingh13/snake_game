b2bbc2cdd4e64c77b95c0e7188ea8130a575daa6
PRIYAsingh13 <ps597934@gmail.com>
2023-02-24 14:50:29 +0530

    game

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // ====== Game constants and variable =====
-const board = document.querySelector('.board')
-let direction = { x: 0, y: 0 };
+const board = document.querySelector(".board");
+let inputDir = { x: 0, y: 0 };
 let speed = 2;
+let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
   {
@@ -9,45 +10,100 @@ let snakeArr = [
     y: 15,
   },
 ];
-food = {x:6,y:7}
+food = { x: 6, y: 7 };
 
 // ====== Game Function ========
 function main(ctime) {
   window.requestAnimationFrame(main);
-  if ((ctime - lastPaintTime) / 1000 < 1/speed) {
+  if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
     return;
   }
   lastPaintTime = ctime;
   gameEngine();
   // console.log(ctime)
 }
+function isCollide(sarr) {
+  return false;
+}
+
 function gameEngine() {
   // update the snake array and food
+  if (isCollide(snakeArr)) {
+    inputDir = { x: 0, y: 0 };
+    alert("Game Over. Press any key to play again!");
+    snakeArr = [{ x: 13, y: 15 }];
+    score = 0;
+  }
+
+//   increment the score and regenerate the food
+if(snakeArr[0].y === food.y &&snakeArr[0].x ===food.x){
+    snakeArr.unshift({x: snakeArr[0].x + inputDir.x,y: snakeArr[0].y + inputDir.y})
+    let a = 2;
+    let b = 16;
+    food = {x:Math.round(a+(b-a)* Math.random()),y:Math.round(a+(b-a)* Math.random())}
+}
+
+// moving the snake
+for(let i = snakeArr.length-2;i>=0;i--){
+    const element = array[i]
+}
 
   // display the snake and food
-//   display the snake
+  //   display the snake
   board.innerHTML = "";
   snakeArr.forEach((e, index) => {
     snakeElement = document.createElement("div");
     snakeElement.style.gridRowStart = e.y;
     snakeElement.style.gridColumnStart = e.x;
     snakeElement.classList.add("snake");
-    if(index === 0){
-        snakeElement.classList.add('head')
+    if (index === 0) {
+      snakeElement.classList.add("head");
+    } else {
+      snakeElement.classList.add("snake");
     }
     board.appendChild(snakeElement);
   });
 
+  // display the food
 
-// display the food
-
-    foodElement = document.createElement("div");
-    foodElement.style.gridRowStart = food.y;
-    foodElement.style.gridColumnStart = food.x;
-    foodElement.classList.add("food");
-    board.appendChild(foodElement);
-//   });
+  foodElement = document.createElement("div");
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add("food");
+  board.appendChild(foodElement);
+  //   });
 }
 
 // ============  Main logic starts here  =======
 window.requestAnimationFrame(main);
+window.addEventListener("keydown", (e) => {
+  inputDir = { x: 0, y: 1 }; //start the game
+  switch (e.key) {
+    case "ArrowUp":
+      console.log("ArrowUp");
+      inputDir.x = 0;
+      inputDir.y = -1;
+      break;
+    case "ArrowDown":
+      console.log("ArrowDown");
+      inputDir.x = 0;
+      inputDir.y = 1;
+
+      break;
+    case "ArrowLeft":
+      console.log("ArrowLeft");
+      inputDir.x = -1;
+      inputDir.y = 0;
+
+      break;
+    case "ArrowRight":
+      console.log("ArrowRight");
+      inputDir.x = 1;
+      inputDir.y = 0;
+
+      break;
+
+    default:
+      break;
+  }
+});
